52bb5fbb3d2c9284091a71eda58ee381b1bee418
BenBktech <bbrucher@gmail.com>
2024-02-25 20:48:39 +0100

    Updated: Mint.tsx, center Spinner

--- frontend/components/Mint.tsx
+++ frontend/components/Mint.tsx
@@ -82,13 +82,15 @@ const Mint = () => {
       width={['100%', '100%', '50%', '50%']}
     >
       {totalSupplyLoading ? (
-        <Spinner
-          thickness='4px'
-          speed='0.65s'
-          emptyColor='gray.200'
-          color='blue.500'
-          size='xl'
-        />
+        <Flex justifyContent="center">
+          <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+          />
+        </Flex>
       ) : (
         <>
           <Flex justifyContent="center">
